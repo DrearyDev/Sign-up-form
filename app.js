b6cf5d3f129d5bb201f9179755369950056894da
drearydev <143649090+DrearyDev@users.noreply.github.com>
2023-11-01 20:57:57 -0400

    Add javascript functionality for password validation

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
 'use strict';
 
+const pass = document.getElementById('password');
+const passError = document.querySelector('.pass-error')
+const confirm = document.getElementById('confirm_password');
+
+let passData = [];
+let confirmData = [];
+
+pass.addEventListener('input', (e) => {
+    if (e.data !== null) {
+        passData.push(e.data);
+    } else {
+        passData.pop();
+    };
+    checkPassword();
+});
+
+confirm.addEventListener('input', (e) => {
+    if (e.data !== null) {
+        confirmData.push(e.data);
+    } else {
+        confirmData.pop();
+    };
+    checkPassword();
+});
+
+function checkPassword() {
+    let password = passData.toString();
+    let confirmPassword = confirmData.toString();
+
+    if (password !== confirmPassword){
+        pass.classList.add('error');
+        passError.style.opacity = '1';
+        confirm.classList.add('error');
+    } else if (password === confirmPassword && passData.length > 0) {
+        pass.classList.remove('error');
+        passError.style.opacity = '0';
+        confirm.classList.remove('error');
+    };
+};
